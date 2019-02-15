d247fe1309a6fa3668f2112874efb46afa9a3d5e
Paul Luna <paulluna0215@gmail.com>
2019-02-14 16:14:09 -0800

    created a simple login

--- index.js
+++ index.js
@@ -16,7 +16,29 @@ app.get('/main.js', function(req, res) {
     res.sendFile(__dirname + '/main.js');
 });
 
+var numUsers = 0;
+
 io.on('connection', function(socket) {
+    var addUser = false;
+
+    socket.on('add user', (username) => {
+        if(addUser) return;
+
+        // we store the username in the socket session for this client
+        socket.username = username;
+        ++numUsers; 
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+
     socket.on('chat message', function(msg) {
         io.emit('chat message', msg);
     })
--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 $(function() {
+    var FADE_TIME = 150; // in ms
+
+    // Initialize variables
+    var $window = $(window);
+    var $usernameInput = $('.usernameInput');
+    var $loginPage = $('.login.page');
+
+    // Propmt for setting a username
+    var username;
+    var connected = false;
+    var $currentInput = $usernameInput.focus();
+
+    const setUsername = () => {
+        username = cleanInput($usernameInput.val().trim());
+
+        // If the username is valid
+        if(username) {
+            $loginPage.fadeOut();
+            $loginPage.off('click');
+
+            // Send username to server
+            socket.emit('add user', username);
+        }
+    }
+
     var socket = io();
     $('form').submit(function(e) {
       e.preventDefault(); // prevents page reloading
@@ -7,5 +32,34 @@ $(function() {
     });
     socket.on('chat message', function(msg){
       $('.messages').append($('<li>').text(msg));
+    })
+
+
+    // Prevents input from having injected markup
+    const cleanInput = (input) => {
+        return $('<div/>').text(input).html();
+    }
+    // Click events
+    
+    // Focus input when clicking anywhere on login page
+    $loginPage.click(() => {
+        $currentInput.focus();
+    });
+
+    // keyboard events
+    $window.keydown(event => {
+        // Auto-focus the current input when a key is typed
+        if(!(event.ctrlKey || event.metaKey || event.altKey)) {
+            $currentInput.focus();
+        }
+
+        // When client hits enter on their keybaord
+        if(event.which === 13) {
+            if(username) {
+                
+            } else {
+                setUsername();
+            }
+        }
     })
   });
