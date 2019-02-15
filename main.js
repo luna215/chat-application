0a8f026c5eab94a80b0c115498f2a0506571d108
Paul Luna <paulluna0215@gmail.com>
2019-02-14 17:26:37 -0800

    other users can see when someone joins the chat

--- main.js
+++ main.js
@@ -4,12 +4,61 @@ $(function() {
     // Initialize variables
     var $window = $(window);
     var $usernameInput = $('.usernameInput');
+    var $messages = $('.messages');
     var $loginPage = $('.login.page');
 
     // Propmt for setting a username
     var username;
     var connected = false;
     var $currentInput = $usernameInput.focus();
+    var socket = io();
+
+    // log a message
+    const log = (message, options) => {
+        var $el = $('<li>').addClass('log').text(message);
+        addMessageElement($el, options)
+    }
+
+    const addMessageElement = (el, options) => {
+        var $el = $(el);
+
+        // setup default options
+        if(!options) {
+            options = {};
+        }
+
+        if(typeof options.fade === 'undefined') {
+            options.fade = true;
+        }
+
+        if(typeof options.prepend === 'undefined') {
+            options.prepend = false;
+        }
+
+        // Apply options
+        if(options.fade) {
+            $el.hide().fadeIn(FADE_TIME);
+        }
+
+        if(options.prepend) {
+            $messages.prepend($el);
+        } else {
+            $messages.append($el);
+        }
+
+        $messages[0].scrollTop = $messages[0].scrollHeight;
+    }
+
+    const addParticipantsMessage = (data) => {
+        var message = '';
+        if(data.numUsers === 1) {
+            message += `there's 1 participant`;
+        } else {
+            message += `there are ${data.numUsers} participants`;
+        }
+
+        log(message);
+    }
 
     const setUsername = () => {
         username = cleanInput($usernameInput.val().trim());
@@ -24,16 +73,23 @@ $(function() {
         }
     }
 
-    var socket = io();
     $('form').submit(function(e) {
       e.preventDefault(); // prevents page reloading
-      socket.emit('chat message', $('#m').val());
-      $('#m').val('');
+      socket.emit('chat message', $('.inputMessage').val());
+      $('.inputMessage').val('');
     });
     socket.on('chat message', function(msg){
       $('.messages').append($('<li>').text(msg));
     })
 
+    socket.on('login', (data) => {
+        connected = true;
+
+        // Display welcome message
+        var message = "Welcome to the chat!";
+        log(message, {prepend: true});
+        addParticipantsMessage(data);
+    });
 
     // Prevents input from having injected markup
     const cleanInput = (input) => {
